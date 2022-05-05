f5ff9f3be9519ad51b689606bd45f87487a2df3a
iryna-m <55385079+iryna-m@users.noreply.github.com>
2022-05-05 15:08:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-open_weather_token = '9833edd1e9ca90f4b147321f01fbf972'
-tg_bot_token = '5352291120:AAHBdYWxZItDKi0guS-opr5WD--_kZPVjN8'
-admin_id = 354003369
+open_weather_token = ''
+tg_bot_token = ''
+admin_id = 
