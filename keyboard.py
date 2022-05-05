12c3470cb3b7b17e9c9468123fc9d294fcfdeb71
Iryna Mikhieieva <medvedevaic@gmail.com>
2022-05-05 15:24:45 +0300

    Rename file with keyboards Add tokens to os

--- keyboard.py
+++ keyboard.py
@@ -3,6 +3,8 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton, message
 
+
+# Create Default Keyboard
 keyboard_start = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 location = types.KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
 keyboard_start.add(location)
@@ -10,3 +12,8 @@ keyboard_start.add(location)
 options = ['Погода на сегодня', 'Погода на завтра', 'Предпочитаю погоду на неделю']
 keyboard_options = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
 keyboard_options.add(*options)
+
+
+# Create Inline button for sharing
+
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import datetime
-import kb
+import keyboard
 import asyncio
 from config import tg_bot_token, open_weather_token
 from aiogram import Bot, types, Dispatcher, executor
@@ -8,16 +8,19 @@ from aiogram.dispatcher.filters import Text
 
 loop = asyncio.get_event_loop()
 bot = Bot(token=tg_bot_token)
+if not tg_bot_token:
+    exit("Error: no token provided")
 dp = Dispatcher(bot, loop=loop)
 
 
 @dp.message_handler(commands='start')
 async def get_location(message: types.Message):
     user_name = message.from_user.first_name
-    msg = dp.message_handler(f'Привет, <b>{user_name}</b>! \n Я расскажу тебе о погоде, ты можешь ввести имя города в поле'
-                        f' ввода или воспользоваться кнопкой', parse_mode='html', reply_markup=kb.keyboard_start)
-    #await message.reply(f'Привет, <b>{user_name}</b>! \n Я расскажу тебе о погоде, ты можешь ввести имя города в поле'
-                        #f' ввода или воспользоваться кнопкой', parse_mode='html', reply_markup=kb.keyboard_start)
+    msg = dp.message_handler(
+        f'Привет, <b>{user_name}</b>! \n Я расскажу тебе о погоде, ты можешь ввести имя города в поле'
+        f' ввода или воспользоваться кнопкой', parse_mode='html', reply_markup=keyboard.keyboard_start)
+    # await message.reply(f'Привет, <b>{user_name}</b>! \n Я расскажу тебе о погоде, ты можешь ввести имя города в поле'
+    # f' ввода или воспользоваться кнопкой', parse_mode='html', reply_markup=kb.keyboard_start)
     bot.next_step_handler(msg, choose_option)
 
 
@@ -27,7 +30,7 @@ async def choose_option(message: types.Message):
     lat = float(message.location.latitude)
     lon = float(message.location.longitude)
     await message.reply(f'<b>{user_name}</b>, \n О какой погоде тебе рассказать?', parse_mode='html',
-                        reply_markup=kb.keyboard_options)
+                        reply_markup=keyboard.keyboard_options)
     # try:
     #     r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={open_weather_token}&units=metric')
     #     data = r.json()
@@ -44,11 +47,12 @@ async def choose_option(message: types.Message):
     #     await message.reply('Мы не можем определить вашу погоду по локации, попробуйте ввести название города в поле ввода')
     #
 
+
 @dp.message_handler()
 async def get_weather_by_city_name(message: types.Message):
     user_name = message.from_user.first_name
     await message.reply(f'<b>{user_name}</b>, \n О какой погоде тебе рассказать?', parse_mode='html',
-                        reply_markup=kb.keyboard_options)
+                        reply_markup=keyboard.keyboard_options)
 
     # global wd
     # code_to_smile = {'Clear' : 'Ясно \U00002600',
@@ -82,7 +86,7 @@ async def get_weather_by_city_name(message: types.Message):
     #     await message.reply('\U0001F449 Проверте правильно ли Вы ввели название города')
 
 
-
 if __name__ == '__main__':
     from handlers import dp
+
     executor.start_polling(dp)
